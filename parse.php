dc3f8757c5bf3312697544f9bd2624ad308febca
Federico Bricker <fbricker@gmail.com>
2015-07-24 22:31:02 -0300

    First commit... about to work :)

---
+++ parse.php
@@ -0,0 +1,109 @@
+<?php
+global $base,$list,$blacklist,$newBlacklist;
+
+$blacklist=array();
+if(file_exists("blacklist.txt")){
+	$blacklist = explode("\n",trim(file_get_contents("blacklist.txt")));
+}
+
+$base = "http://wiki.dokkogroup.com.ar/convert/?page=";
+$newBlacklist = array();
+@mkdir("data");
+
+function isBlacklisted($page){
+	global $blacklist;
+	if(strpos($page,"http://")===0) return true;
+	if(strpos($page,"https://")===0) return true;
+	return in_array(utf8_encode($page), $blacklist);
+}
+
+function getDataBetweenTokens($data,$open,$close){
+	$arr = explode($open," ".$data);
+	unset($arr[0]);
+	$res = array();
+	foreach($arr as $part){
+		$aux = explode($close,$part,2);
+		if(count($aux)==1) continue;
+		if(substr_count($aux[0],"\n")>0) continue;
+		if(isBlacklisted($aux[0])) continue;
+		$res[]=$aux[0];
+	}
+	return $res;
+}
+
+function parsePage($data){
+	$links1 = getDataBetweenTokens($data,"[[","]]");
+	$links2 = getDataBetweenTokens($data,"](",")");
+	$links3 = getDataBetweenTokens($data,"|","]");
+	$res = array_merge($links1,$links2,$links3);
+	foreach ($res as $name) addPage($name);
+}
+
+function removeAccents($cadena){
+	$tofind = utf8_decode("ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ");
+	$replac = "AAAAAAaaaaaaOOOOOOooooooEEEEeeeeCcIIIIiiiiUUUUuuuuyNn";
+	return(strtr($cadena,$tofind,$replac));
+}
+
+function normalizeName($name){
+    $name = strip_tags(str_replace(array("\r","\t","\n","@",":",";",'"',"'"," ","“","”","`","´","?","¿","%","/"),'-',$name));
+    return removeAccents(preg_replace('/[\-]{2,}/','-',$name));
+}
+
+function getFileName($page){
+	return "data/".$page['normal'].".md";
+}
+
+function getPage($page){
+	global $base;
+	$data = file_get_contents($base.urlencode(utf8_decode(($page['name']))));
+	savePage($page,$data);
+	return $data;
+}
+
+function savePage($page,$data){
+	global $newBlacklist;
+	echo "salvando ".$page['name']." como: ".$page['normal']."\n";
+	if(empty($data)) {
+		echo "[WARNING] la pagina esta vacia (talvez la quieras poner en blacklist?)\n";
+		$newBlacklist[]=$page['name'];
+		return;
+	}
+	file_put_contents(getFileName($page),$data);
+}
+
+function exists($page){
+	return file_exists(getFileName($page));
+}
+
+function addPage($page){
+	global $list;
+	if(empty($list)) $list=array();
+	$normal = normalizeName($page);
+	if(!isset($list[$normal])) $list[$normal]=array("name"=>utf8_encode($page),"normal"=>$normal);
+}
+
+@$list = json_decode(file_get_contents("database.txt"),true);
+addPage("Denko");
+
+foreach($list as $page){
+	if(!exists($page)) {
+		$data = getPage($page);
+	}else{
+		$data = file_get_contents(getFileName($page));
+	}
+	parsePage($data);
+}
+
+echo "listo!!!\n";
+if(count($newBlacklist)>0){
+	echo "\nHay nuevas blacklist:\n";
+	print_r($newBlacklist);
+	$nb = "";
+	foreach($newBlacklist as $new){
+		$nb.="$new\n";
+	}
+	file_put_contents("newBlacklist.txt", $nb);
+}
+
+file_put_contents("database.txt",json_encode($list,true));
---
+++ phpwikiSctipts/index.php
@@ -0,0 +1,100 @@
+<?php
+global $replaces,$repCount;
+$replaces = array();
+$repCount = 0;
+$db = 'phpwiki';
+$dbUser = 'root';
+$dbPass = '1234';
+
+function guessLang($data){
+	$html = substr_count($data,'>');
+	$html+= substr_count($data,'<');
+	$smarty = substr_count("{/");
+	$smarty+= substr_count("{\$");
+	$php = substr_count($data,";\n");
+	$php+= substr_count($data,"(\$");
+	
+	if($php>=$html && $php>$smarty) return 'php';
+	if($smarty+$html > 5) return 'smarty';
+	return '';
+}
+
+function saveDataBetweenTokens($data,$open,$close){
+	global $replaces,$repCount;
+	$arr = explode($open," ".$data);
+	unset($arr[0]);
+	foreach($arr as $part){
+		$aux = explode($close,$part,2);
+		if(count($aux)==1) continue;
+		$newClose='```';
+		$newOpen='```'.guessLang($aux[0]);
+		
+		$k="REPLACEMMMMREPLACEMMM".$repCount++."KKKK";
+		$data=str_replace($open.$aux[0].$close,$k,$data);
+		$aux[0]=str_replace('~//','//',$aux[0]);
+		$aux[0]=str_replace('\'~','\'',$aux[0]);
+		$replaces[$k]=$newOpen.$aux[0].$newClose;
+	}
+	return $data;
+}
+
+function replaceCodeBlocks($data){
+	$data = saveDataBetweenTokens($data,"<pre>","</pre>");
+	$data = saveDataBetweenTokens($data,"<verbatim>","</verbatim>");
+	return $data;
+}
+
+function restoreDataBetweenTokens($data){
+	global $replaces;
+	foreach($replaces as $k=>$v) $data=str_replace($k,$v,$data);
+	return $data;
+}
+
+$page = $_GET['page'];
+
+$res = exec("echo \"select content from version where id in (select id from page where pagename = '".mysql_escape_string($page)."') order by version desc, minor_edit desc limit 1;\" | mysql -u $dbUser -p$dbPass $db");
+$res .="\n";
+
+$file = "tmp_".md5($page).".tmp";
+$res = str_replace(array("%%%","\\n","<br>","<br/>"),"\n",$res);
+$res = str_replace(array("\r"),"",$res);
+$res = replaceCodeBlocks($res);
+$res = str_replace(array("<b>","</b>"),"**",$res);
+$res = preg_replace("/^\!/m","\n!",$res);
+
+$tablas = explode("<?plugin OldStyleTable",$res);
+$res = $tablas[0];
+unset($tablas[0]);
+
+foreach($tablas as $t){
+	list($tabla,$resto)=explode("?>",$t,2);
+	$tabla = trim($tabla);
+	$rows=explode("\n",$tabla);
+	$res.="\n".$rows[0]."|\n";
+	$res.=preg_replace("/([^\|])/","-",$rows[0])."|\n";
+	unset($rows[0]);
+	foreach($rows as $row){
+		$res.=$row."|\n";
+	}
+//$tabla=preg_replace("/^\|/m","<tr><td>",$tabla);
+//	$tabla=preg_replace("/$/m","|",$tabla);
+//	$tabla=preg_replace("/\|/","</td><td>",$tabla);
+	$res.="\n".$resto;
+}
+
+file_put_contents($file,$res);
+exec("./wiki2github ".$file);
+
+$res = file_get_contents($file);
+unlink($file);
+
+$res = str_replace("\n\n\n","\n\n",$res);
+$res = str_replace("\n\n\n","\n\n",$res);
+$res = str_replace("\n\n\n","\n\n",$res);
+$res = str_replace("\n\n\n","\n\n",$res);
+$res = str_replace("\n\n\n","\n\n",$res);
+$res = str_replace("\n\n\n","\n\n",$res);
+$res = restoreDataBetweenTokens($res);
+
+session_start();
+echo $res;
